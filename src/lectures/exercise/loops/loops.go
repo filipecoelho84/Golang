a1efc3afa007880f485ad7746972d97e67fb59d1
Filipe Coelho <coelho.sox@gmail.com>
2022-02-27 17:33:12 +0000

    You are doing good ;)

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -12,7 +12,22 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	for i := 1; i <= 50; i++ {
+
+		if i%3 == 0 && i%5 != 0 {
+			fmt.Println("Fizz")
+		} else if i%3 != 0 && i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
