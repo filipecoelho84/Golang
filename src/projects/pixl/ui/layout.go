ae838a25c792b76b0e5ff5bc98480b02d84e1787
Filipe Coelho <coelho.sox@gmail.com>
2022-04-01 22:47:01 +0100

    Pixl Project

--- src/projects/pixl/ui/layout.go
+++ src/projects/pixl/ui/layout.go
@@ -1,7 +1,12 @@
 package ui
 
+import "fyne.io/fyne/v2/container"
+
 func Setup(app *AppInit) {
 	swatchsContainer := BuildSwatches(app)
+	colorPicker := SetupColorPicker(app)
 
-	app.PixlWindow.SetContent(swatchsContainer)
+	appLayout := container.NewBorder(nil, swatchsContainer, nil, colorPicker)
+
+	app.PixlWindow.SetContent(appLayout)
 }
